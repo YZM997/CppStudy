2754edd7b2bb63fd720b51e363fde9a0467613c9
YZM997 <20252082065@mail.dlut.edu.cn>
2026-06-30 21:11:36 +0800

    test

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+
+    getchar();    // 等待按回车
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+
+    cin.get();   // 等待按回车（类似 getchar）
+
+    return 0;
+}
